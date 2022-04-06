d464c4e66439731c213ffe8a11d04a6fa9144fad
Qwabena <100248770+Qwabena-Proxy@users.noreply.github.com>
2022-04-06 17:07:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ def number_check(number) -> tuple:
   max_number = max(number)
   max_number = min(number)
   
-  
+  return f'{max_number} is the maximum number and {min_number} is the minimum number.'
