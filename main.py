8f39861fe89ae381c0182acf8cf6e573de4ff052
Qwabena <100248770+Qwabena-Proxy@users.noreply.github.com>
2022-04-06 17:14:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,6 @@ def number_check(number) -> tuple:
   max_number = min(number)
   
   return f'{max_number} is the maximum number and {min_number} is the minimum number.'
+
+entry = input("Enter data:").split(",")
+print(number_check(entry))
